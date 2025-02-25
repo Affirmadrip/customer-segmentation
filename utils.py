ae93030f727c576248ca8637686ddce56912b543
Affirmadrip <158172593+Affirmadrip@users.noreply.github.com>
2025-02-25 20:38:01 +0700

    8th commit

--- utils.py
+++ utils.py
@@ -1,22 +1,17 @@
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from PIL import Image
+# image_utils.py
+def get_image_url(item):
+    # Dictionary mapping items to their respective image URLs
+    image_urls = {
+        "Dress": "https://zapaka.com/cdn/shop/products/04033012-Purplefirst.jpg?v=1628925127",
+        "Clothing": "https://www.thoughtco.com/thmb/ctxxtfGGeK5f_-S3f8J-jbY-Gp8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/close-up-of-clothes-hanging-in-row-739240657-5a78b11f8e1b6e003715c0ec.jpg",
+        "Jewelry": "https://www.buccellati.com/media/catalog/category/1_High_Jewelry.jpg",
+        "Belt": "https://wildrhinoshoes.com.au/cdn/shop/files/Belts_1.jpg?v=1708316630",
+        "Shirt": "https://www.mrporter.com/variants/images/1647597332681244/pr/w1000.jpg",
+        # Add more mappings as needed
+    }
+    return image_urls.get(item, "https://example.com/default.jpg")  # Default image if not found
 
-def preprocess_data(data):
-    numerical_features = ['Age', 'Purchase Amount (USD)', 'Review Rating', 'Previous Purchases']
-    categorical_features = ['Gender', 'Category', 'Item Purchased']
-    numerical_transformer = StandardScaler()
-    categorical_transformer = OneHotEncoder()
-
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('num', numerical_transformer, numerical_features),
-            ('cat', categorical_transformer, categorical_features)
-        ])
-    
-    return preprocessor.fit_transform(data), preprocessor
-
-def resize_image(image, base_width):
-    w_percent = (base_width / float(image.size[0]))
-    h_size = int((float(image.size[1]) * float(w_percent)))
-    return image.resize((base_width, h_size), Image.Resampling.LANCZOS)
+def display_image(st, item, width=1080):
+    """Display an image given an item category using Streamlit."""
+    url = get_image_url(item)
+    st.image(url, caption=item, width=width)

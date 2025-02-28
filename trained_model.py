242cc1cc0fde59f3923607413539c87d2dd285c2
Affirmadrip <158172593+Affirmadrip@users.noreply.github.com>
2025-02-28 22:25:11 +0700

    30th commit

--- trained_model.py
+++ trained_model.py
@@ -1,17 +1,15 @@
 import pandas as pd
-import pickle
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.cluster import KMeans
+import pickle
 
 # Load data
 data = pd.read_csv('shopping_trends.csv')
 
-# Define feature columns
+# Preprocessing for clustering
 numerical_features = ['Age', 'Purchase Amount (USD)', 'Review Rating', 'Previous Purchases']
 categorical_features = ['Gender', 'Category', 'Item Purchased']
-
-# Define preprocessors
 numerical_transformer = StandardScaler()
 categorical_transformer = OneHotEncoder()
 
@@ -21,16 +19,16 @@ preprocessor = ColumnTransformer(
         ('cat', categorical_transformer, categorical_features)
     ])
 
-# Fit and transform the data
 data_processed = preprocessor.fit_transform(data)
-
-# Train K-Means model
 kmeans = KMeans(n_clusters=4, random_state=42)
-kmeans.fit(data_processed)
+data['Cluster'] = kmeans.fit_predict(data_processed)
 
-# Save the preprocessor and model as pickle files
+# Save the preprocessor and the model
 with open('preprocessor.pkl', 'wb') as f:
     pickle.dump(preprocessor, f)
 
 with open('kmeans_model.pkl', 'wb') as f:
     pickle.dump(kmeans, f)
+
+# Save the modified data with clusters
+data.to_csv('shopping_trends.csv', index=False)
